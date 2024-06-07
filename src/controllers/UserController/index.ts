b392b3e40da95994835e7b10f3d18acc7aab00f4
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2024-06-07 15:43:20 +0700

    :recycle: Simplify zod error message

--- src/controllers/UserController/index.ts
+++ src/controllers/UserController/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
-import db from "../../db";
-import Response from "../../utils/response";
+import db from "~/db";
+import Response from "~/utils/response";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 import { hashSync } from "bcrypt-edge";
@@ -31,36 +31,39 @@ const schema = z
     path: ["password_confirmation"],
   });
 
-const createUserValidation = zValidator("json", schema);
-
-const UserController = new Hono();
-
-/**
- * get user lists
- */
-UserController.get("/", async (c) => {
-  const users = await db(c.env).users.findMany();
-  return new Response(c).success(users);
-});
-
-/**
- * create user
- */
-UserController.post("/", createUserValidation, async (c) => {
-  const request = c.req.valid("json");
-  const hashedPassword = hashSync(request.password, 8);
-  try {
-    await db(c.env).users.create({
-      data: {
-        password: hashedPassword,
-        email: request.email ?? "",
-        name: request.name ?? "",
-      },
-    });
-  } catch (error: any) {
-    return new Response(c).error(error);
+const createUserValidation = zValidator("json", schema, (result, c) => {
+  if (!result.success) {
+    return new Response(c).error(result.error);
   }
-  return c.json(request);
 });
+
+const UserController = new Hono()
+  /**
+   * get user lists
+   */
+  .get("/", async (c) => {
+    const users = await db(c.env).users.findMany();
+    return new Response(c).success(users);
+  })
+
+  /**
+   * create user
+   */
+  .post("/", createUserValidation, async (c) => {
+    const request = c.req.valid("json");
+    const hashedPassword = hashSync(request.password, 8);
+    try {
+      await db(c.env).users.create({
+        data: {
+          password: hashedPassword,
+          email: request.email ?? "",
+          name: request.name ?? "",
+        },
+      });
+    } catch (error: any) {
+      return new Response(c).error(error);
+    }
+    return c.json(request);
+  });
 
 export default UserController;
--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,5 +1,6 @@
 import { Context } from "hono";
 import { StatusCode } from "hono/utils/http-status";
+import { ZodError } from "zod";
 
 class Response {
   context: Context<any, any, {}>;
@@ -7,6 +8,31 @@ class Response {
   constructor(c: Context) {
     this.context = c;
   }
+
+  /**
+   * check if object is came from Zod
+   * @param obj
+   * @returns boolean
+   */
+  isZodError = (obj: ZodError) => {
+    return obj.name === "ZodError" && Array.isArray(obj.issues);
+  };
+
+  /**
+   * Mapping zod error to be more simple
+   * @param error
+   * @returns array of object {field, message}
+   */
+  mapZodError = (error: ZodError) => {
+    return error.issues.map((issue) => {
+      const field = issue.path.join(".");
+      const message = issue.message;
+      return {
+        field,
+        message,
+      };
+    });
+  };
 
   success = (data: any) => {
     return this.context.json({
@@ -19,7 +45,7 @@ class Response {
     return this.context.json(
       {
         success: false,
-        error,
+        error: this.isZodError(error) ? this.mapZodError(error) : error,
       },
       statusCode ?? 400
     );
