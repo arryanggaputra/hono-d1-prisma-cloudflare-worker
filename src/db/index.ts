e70b82c28a137d3a758ccb35698b55bd8f3c74a4
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2024-06-09 18:59:27 +0700

    :recycle: change binding param

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaD1 } from "@prisma/adapter-d1";
+import { Bindings } from "~/types";
 
-export interface Env {
-  DB: D1Database;
-}
-
-// use `prisma` in your application to read and write data in your DB
-
-const db = (env: any) => {
+const db = (env: Bindings) => {
   const adapter = new PrismaD1(env.DB);
   const prisma = new PrismaClient({ adapter, errorFormat: "minimal" });
   return prisma;
